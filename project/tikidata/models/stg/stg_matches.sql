0a35bb8464bfce5c7fa1e8f58c591a29b066a3e0
Abdelrhmankaram <abdelrhmankaram171@gmail.com>
2026-04-27 22:11:48 +0300

    Add staging models for matches, players, and standings

---
+++ project/tikidata/models/stg/stg_matches.sql
@@ -0,0 +1,23 @@
+with stg_matches as (
+    select * from RAW.DBT_DEV_RAW_TABLES.MATCHES_TABLE
+)
+
+select 
+    ROW_NUMBER() over (order by "date") as match_id,
+    "date" as match_date,
+    "time" as match_time,
+    "team" as team_id,
+    "opponent" as opponent_team_id,
+    "round" as round,
+    "venue" as venue,
+    "GF" as "goals_scored",
+    "GA" as "goals_conceded",
+    "Poss" as possession,
+    "Attendance",
+    "Captain" as player_id,
+    "Formation" as team_formation_id,
+    "Opp Formation" as opponent_formation_id,
+    "season_2" as season_id,
+    "league" as league_id
+from
+    stage_matches
---
+++ project/tikidata/models/stg/stg_players.sql
@@ -0,0 +1,16 @@
+with stg_players as (
+    select * from RAW.DBT_DEV_RAW_TABLES.PLAYERS_DATA
+)
+
+select
+    "Player" as player_name,
+    "Nation" as country_id,
+    "Rk" as rank,
+    split("Pos", ',')[0] as main_position,
+    split("Pos", ',')[1] as second_position,
+    "Squad" as team_id,
+    "Season" - "Age" -1 as birth_year,
+    "Weekly Wages" as weekly_wages,
+    "Season" as season_id
+from
+    stg_players

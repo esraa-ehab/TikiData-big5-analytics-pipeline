c4b05ad38f04c80e2d516f1a0791e188bd8d44ff
Abdelrhmankaram <abdelrhmankaram171@gmail.com>
2026-05-17 22:50:00 +0300

    Refactor fact_standing SQL model to use unique_player_sk and change joins to left joins
    - Updated player and goalkeeper references to use unique_player_sk instead of player_sk.
    - Changed inner joins to left joins for team, season, league, and player dimensions to allow for more flexible data retrieval.

--- project/tikidata/models/marts/facts/fact_standing.sql
+++ project/tikidata/models/marts/facts/fact_standing.sql
@@ -1,12 +1,13 @@
 with fact_standing as (
     select 
+        p.player_id,
         {{ dbt_utils.generate_surrogate_key(['standing_id']) }} as standings_sk,
         lg.league_sk,
         szn.season,
         tm.team_sk,
-        p.player_sk as top_scorer_sk,
+        p.unique_player_sk as top_scorer_sk,
         stnd.top_scorer_goals,
-        gk.player_sk as goalkeaper_sk,
+        gk.unique_player_sk as goalkeaper_sk,
         stnd.rank,
         stnd.points,
         stnd.matches_played,
@@ -19,15 +20,18 @@ with fact_standing as (
         stnd.attendance,
         
     from {{ ref('int_standings') }} as stnd
-    join {{ ref('dim_player') }} as p
-        on stnd.top_scorer_id = p.player_id
-    join {{ ref('dim_player') }} as gk
-        on stnd.goalkeeper_id = gk.player_id
-    join {{ ref('dim_team') }} as tm
+    left join {{ ref('dim_team') }} as tm
         on stnd.team_id = tm.team_id
-    join {{ ref('int_seasons') }} as szn
+    left join {{ ref('int_seasons') }} as szn
         on stnd.season_id = szn.season_id
-    join {{ ref('dim_league') }} as lg
+
+    left join {{ ref('dim_league') }} as lg
         on stnd.league_id = lg.league_id
+
+    left join {{ ref('dim_unique_player') }} as p
+        on p.player_id = stnd.top_scorer_id
+
+    left join {{ ref('dim_unique_player') }} as gk
+        on gk.player_id = stnd.goalkeeper_id
 )
 select * from fact_standing
